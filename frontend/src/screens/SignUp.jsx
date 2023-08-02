515d1a7dd75cc2d6f0daf30a10912a9b5644fca2
Namit-me <namitme2002@gmail.com>
2023-08-03 02:25:35 +0530

    .

--- frontend/src/screens/SignUp.jsx
+++ frontend/src/screens/SignUp.jsx
@@ -13,7 +13,7 @@ export default function Signup() {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    const response = await fetch("http://localhost:5000/api/createuser", {
+    const response = await fetch("https://gofood-foodorderingapp.onrender.com/api/createuser", {
       // credentials: 'include',
       // Origin:"http://localhost:3000/login",
       method: "POST",
