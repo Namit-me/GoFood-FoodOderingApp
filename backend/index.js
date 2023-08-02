f0c042ee18ee8f3b20b5bb44fe8a0539be377231
Namit-me <namitme2002@gmail.com>
2023-08-03 02:36:36 +0530

    indev update

--- backend/index.js
+++ backend/index.js
@@ -7,7 +7,7 @@ const connectDB = require("./mongoDB/db")
 
 
 app.use((req, res, next) =>{
-  res.setHeader('Access-Control-Allow-Origin', "http://localhost:3000");
+  res.setHeader('Access-Control-Allow-Origin', "https://chimerical-caramel-bec29d.netlify.app/");
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
